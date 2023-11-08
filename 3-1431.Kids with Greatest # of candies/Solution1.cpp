007f45c5b4e6a59ebc88fef001ce04a9d8136a69
MahdisRezaei2 <mahdis.re20@gmail.com>
2023-11-07 18:18:24 -0800

    Commit my solutions for the 3rd challegne of Leetcode data structures

--- 3-1431.Kids with Greatest # of candies/Solution1.cpp
+++ 3-1431.Kids with Greatest # of candies/Solution1.cpp
@@ -3,46 +3,32 @@
 #include <vector>
 using namespace std;
 
-vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
-{
-   
-   int greatestNum;
-   int maximum;
-   vector<bool> result( candies.size(), true);
 
-    
-    for (int i=0; i < candies.size() ; i++){
-         maximum= candies[i]+extraCandies ;
-         
-         for(int j=1; j < candies.size(); j++){
-           
-             if(maximum >= candies[j]){
-                greatestNum= maximum;
-             }
-             else{
-              greatestNum = candies[j];
-              result[i]=false;
-                
-             } 
-        
-         }
-       
-       
+///greates is who is greater than the greatest 
+vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+{   
+    vector<bool> result;
+    int maxNumberOfCandies=0;
+    for(int i=0 ; i < candies.size(); i++){
+        maxNumberOfCandies = max(maxNumberOfCandies, candies[i]);
+    }
+    //cout<<maxNumberOfCandies;
+
+    for (int j=0; j < candies.size();j++){
+        result.push_back(candies[j]+extraCandies >= maxNumberOfCandies);
+        //cout<<result[j];
     }
     return result;
+    
 }
-
-
-
 
 int main()
 {
-   vector<int> candies = {12,1,12} ;
-   int extraCandies =10;
-   vector<bool> result = kidsWithCandies(candies , extraCandies);
-   //cout<<result.size();
-   for(int i=0; i < result.size();i ++){
-    cout<<result[i]<<" ";
-   }
+    vector<int> candies = {4,2,1,1,2};
+    int extraCandies = 1;
+    vector<bool> result = kidsWithCandies(candies, extraCandies);
+    for(int i=0 ; i < candies.size(); i++){
+        cout<<result[i];
+    }
     return 0;
 }
